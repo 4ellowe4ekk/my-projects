f9e3db561d98ae542b356dec72b8e8271c625c1b
4ellowe4ekk <4ellowe4ek999@gmail.com>
2026-05-15 22:31:45 +0300

    Улучшил код!

--- calculator.py
+++ calculator.py
@@ -1,1 +1,15 @@
-print(eval(input()))
+print("Калькулятор")
+
+while True:
+
+    def calc():
+    
+        try:
+
+            print(eval(input()))
+        
+        except Exception as e:
+            print("Ошибка!")
+
+    calc()
+    
